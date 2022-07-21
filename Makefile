0e5221eeda301034747d1c15ff47f8464a5f37d8
Marco Fenucci <marco_fenucci@matf.bg.ac.rs>
2022-07-21 14:22:45 +0000

    Added some structure in the gamma_est_mc_batches.sh script

--- Makefile
+++ Makefile
@@ -6,11 +6,14 @@ include make.flags
 SRC0=gamma_est_mc.f90 used_const.f90 yarko_force.f90
 EXE0=gamma_est_mc.x
 
-SRC1=yarko_compare.f90 used_const.f90 yarko_force.f90
-EXE1=yarko_compare.x
+SRC1=gamma_est_mc_threadsafe.f90 used_const.f90 yarko_force.f90
+EXE1=gamma_est_mc_threadsafe.x
 
-SRC2=gamma_est_mc_threadsafe.f90 used_const.f90 yarko_force.f90
-EXE2=gamma_est_mc_threadsafe.x
+SRC2=yarko_grid_rho_D.f90 used_const.f90 yarko_force.f90
+EXE2=yarko_grid_rho_D.x
+
+SRC3=yarko_compare.f90 used_const.f90 yarko_force.f90
+EXE3=yarko_compare.x
 
 #########################################################
 # liste of object files with /obj prefix
@@ -24,12 +27,15 @@ OBJ1=$(addprefix .obj/,$(OBJ_NOPREFIX1))
 OBJ_NOPREFIX2=$(SRC2:.f90=.o)
 OBJ2=$(addprefix .obj/,$(OBJ_NOPREFIX2))
 
+OBJ_NOPREFIX3=$(SRC3:.f90=.o)
+OBJ3=$(addprefix .obj/,$(OBJ_NOPREFIX3))
+
 BIN=bin
 
 ################################################################
 # creation of executable programs
 
-all: $(BIN)/$(EXE0) $(BIN)/$(EXE1) $(BIN)/$(EXE2) links_ex
+all: $(BIN)/$(EXE0) $(BIN)/$(EXE1) $(BIN)/$(EXE2) $(BIN)/$(EXE3) links_ex
 
 $(BIN)/$(EXE0): $(OBJ0)
 	$(FC) $(FFLAGS) $^ -o $@ $(LINK)
@@ -40,11 +46,15 @@ $(BIN)/$(EXE1): $(OBJ1)
 $(BIN)/$(EXE2): $(OBJ2)
 	$(FC) $(FFLAGS) $^ -o $@ $(LINK)
 
+$(BIN)/$(EXE3): $(OBJ3)
+	$(FC) $(FFLAGS) $^ -o $@ $(LINK)
+
 links_ex:
 	rm -rf *.x 2>/dev/null 
 	ln -s bin/$(EXE0)
 	ln -s bin/$(EXE1)
 	ln -s bin/$(EXE2)
+	ln -s bin/$(EXE3)
 
 #########################################################
 # compilation of sources
@@ -53,6 +63,9 @@ links_ex:
 #	$(FC) $(FCFLAGS) -c $< -o $@
 
 .obj/yarko_compare.o: src/yarko_compare.f90 .obj/yarko_force.o .obj/used_const.o
+	$(FC) $(FFLAGS) -c $< -o $@
+
+.obj/yarko_grid_rho_D.o: src/yarko_grid_rho_D.f90 .obj/yarko_force.o .obj/used_const.o
 	$(FC) $(FFLAGS) -c $< -o $@
 
 .obj/gamma_est_mc.o: src/gamma_est_mc.f90 .obj/yarko_force.o .obj/used_const.o
--- gamma_est_mc_batches.sh
+++ gamma_est_mc_batches.sh
@@ -1,5 +1,34 @@
 #/bin/bash
 
+############################################################
+#      AUTHOR: Marco Fenucci                               #
+#        DATE: Agust 2022                                  #
+# INSTITUTION: University of Belgrade                      #
+#                                                          #
+# This script lanches the thermal inertia estimation in    #
+# parallel for a list of objects, each of them with 1 CPU  #
+#                                                          #
+############################################################
+
+# TODO: This script must be improved:
+#  1) Input distributions of the objects must be placed in a folder
+#     called data
+#  2) Instead of running all the objects in data, we should provide 
+#     a list of objects to run, maybe including their semimajor axis and eccentricity
+#  3) Instead of using fixed input files for the main program, I think we should give
+#     in input other arguments to choose the options:
+#        -nproc=        (mandatory)
+#        -C=            (optional)
+#        -Kmin=         (optional)
+#        -Kmax=         (optional)
+#        -method=       (optional)
+#        -max_iter=     (optional)
+#        -expo=         (optional)
+#        -epsi=         (optional)
+#        -alpha=        (optional)
+# For the optional arguments, we should assign some default value
+
+
 function usage () {
    echo " Usage: gamma_est_mc_par.x <nproc>"
    exit 1
@@ -19,7 +48,6 @@ rm * 2> /dev/null
 ls -d */ | sed -e "s/\///g" > directory_list
 cd ..
 
-usedproc=0
 while IFS=" " read -r dir ; do
    cd data
 
@@ -44,14 +72,17 @@ while IFS=" " read -r dir ; do
 
    sleep 1
 
-   let usedproc=usedproc+1
-   if [ $usedproc -ge $nproc ]; then
-     wait
-     usedproc=0
-   fi
+   # Take the number of processes running. If nproc processes
+   # are running, check every 10 seconds if some of them has finished.
+   # In this manner we almost always keep occupied nproc processes
+   currRunning=$(ps -el | grep gamma_est_mc.x | wc -l)
+   while [ $currRunning -ge $nproc ]; do
+      sleep 10
+      currRunning=$(ps -el | grep gamma_est_mc.x | wc -l)
+   done
+
 done < data/directory_list 
 
 wait
 
 cd ..
-
