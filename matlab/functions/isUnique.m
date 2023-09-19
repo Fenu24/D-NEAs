07917da130094e454cdd3d1ac0ce4f4ff14bcce3
Marco Fenucci <marco_fenucci@matf.bg.ac.rs>
2023-09-19 11:20:27 +0200

    Removed matlab functions

--- matlab/functions/isUnique.m
+++
@@ -1,8 +0,0 @@
-function res = isUnique(Namelist, name)
-   index = searchName(Namelist, name);
-   if(length(index)==1)
-      res = true;   
-   else
-      res = false;
-   end
-end
--- matlab/granvikModel/gmb_model_construct.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-cat gmb_model1.dat >  gmb_model.dat
-cat gmb_model2.dat >> gmb_model.dat
-cat gmb_model3.dat >> gmb_model.dat
-cat gmb_model4.dat >> gmb_model.dat
-cat gmb_model5.dat >> gmb_model.dat
-cat gmb_model6.dat >> gmb_model.dat
